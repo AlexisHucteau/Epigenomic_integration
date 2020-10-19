981979feb60f4b812d5835c6d20f0daace7a3141
AlexisHucteau <alexis.hucteau@inserm.fr>
2020-10-19 16:21:38 +0200

    Starting analysis script

--- Scripts/functions.R
+++ Scripts/functions.R
@@ -1,1 +1,35 @@
-print("hello")
+Differential_analysis <- function(Focused_variable, DATA){
+  design.pairs <- function(levels) {
+    n <- length(levels)
+    design <- matrix(0,n,choose(n,2))
+    rownames(design) <- levels
+    colnames(design) <- 1:choose(n,2)
+    k <- 0
+    for (i in 1:(n - 1))
+      for (j in (i + 1):n) {
+        k <- k + 1
+        design[i,k] <- 1
+        design[j,k] <- -1
+        colnames(design)[k] <- paste(levels[i], "-", levels[j],sep = "")
+      }
+    design
+  }
+  design <- model.matrix(~0 + Focused_variable)
+  contr.matrix <- design.pairs(levels(factor(Focused_variable)))
+  colnames(design) <- rownames(contr.matrix)   
+  Fit <- lmFit(DATA, design) %>%
+    contrasts.fit(., contr.matrix) %>%
+    eBayes(., trend = TRUE)
+  
+  FitList <- list()
+  for (i in 1:ncol(contr.matrix)) {
+    FitList[[i]] <- topTable(Fit, coef = i, adjust.method = "BH", number = nrow(DATA)) %>%
+      mutate(ID = rownames(.))
+    
+    message(paste0(i, " done"))
+    
+  }
+  names(FitList) <- colnames(contr.matrix)
+  return(FitList)
+  
+}
